6d6ad5bc81303c83a50547c6cb1f3c1a0da56b7a
Huy Pham Ngoc (VN) <Huy.PhamN1@homecredit.vn>
2024-08-23 16:52:48 +0700

    Update new version with odbc connect

--- Automate_sum.py
+++ Automate_sum.py
@@ -13,15 +13,19 @@ def combine_find(df, target_sum, column_index, column_sum):
                 result.append(current_indices[:])
                 logging.info(f"Found combination: {current_indices}")
                 return
+
             if current_sum > target_sum:
-                return        
+                return   
+                 
             for i in range(start, len(df)):
                 current_indices.append(df[column_index][i])
                 backtrack(i + 1, current_sum + df[column_sum][i], current_indices)
                 current_indices.pop()
+                if current_sum == target_sum:
+                    break
         except Exception as e:
             logging.error(f"Error occurred: {e}")
-    
+
     backtrack(0, 0, [])
     return result
 
@@ -36,8 +40,7 @@ def main_combination_result(df, target_sum):
                 logging.info(f"Indices that sum to {target_sum}: {combination}")
                 matching_rows = df[df[column_index].isin(combination)]
                 df_new.append(matching_rows)
-            #return pd.concat(df_new).reset_index(drop=True)
-            return pd.DataFrame(df_new[1].copy())
+            return pd.concat(df_new).reset_index(drop=True)
         else:
             raise ValueError("No combinations match the target sum.")
     except ValueError as ve:
@@ -46,12 +49,13 @@ def main_combination_result(df, target_sum):
 
 def main():
     data = {
-        'index': np.arange(1, 201),
-        'amount': np.random.randint(1, 100, size=200)  
+        'index': np.arange(1, 101),
+        'amount': np.random.randint(1, 100, size=100)  
     }
     df = pd.DataFrame(data)
     target_sum = 23
     result_df = main_combination_result(df, target_sum)
+    logging.info(f"Done Finding Combination Sum")
     print(result_df)
 
 if __name__ == "__main__":
---
+++ query.py
@@ -0,0 +1,1 @@
+query = 'Select * from database when ____'
